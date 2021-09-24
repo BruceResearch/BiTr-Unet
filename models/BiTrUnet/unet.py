5710bf59e21bf73e582a291b5394934a837ad683
Bruce <78635323+JustaTinyDot@users.noreply.github.com>
2021-09-24 13:53:21 -0400

    Add files via upload

---
+++ models/BiTrUnet/unet.py
@@ -0,0 +1,283 @@
+#Adapted from https://github.com/lescientifik/open_brats2020
+"""A small Unet-like zoo"""
+import torch
+from torch import nn
+import torch.nn.functional as F
+#from torch.utils.checkpoint import checkpoint_sequential
+
+from models.TransBTS.layers import ConvBnRelu, UBlock, conv1x1, UBlockCbam, CBAM
+
+
+class InitConv(nn.Module):
+    def __init__(self, in_channels=4, out_channels=16, dropout=0.2):
+        super(InitConv, self).__init__()
+
+        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.dropout = dropout
+
+    def forward(self, x):
+        y = self.conv(x)
+        y = F.dropout3d(y, self.dropout)
+
+        return y
+
+class Unet(nn.Module):
+    """Almost the most basic U-net.
+    """
+    name = "Unet"
+
+    def __init__(self, inplanes, num_classes, width, norm_layer=None, deep_supervision=False, dropout=0,
+                 **kwargs):
+        super(Unet, self).__init__()
+        features = [width * 2 ** i for i in range(4)]
+        print(features)
+
+        self.deep_supervision = deep_supervision
+
+        self.encoder1 = UBlock(inplanes, features[0] // 2, features[0], norm_layer, dropout=dropout)
+        self.encoder2 = UBlock(features[0], features[1] // 2, features[1], norm_layer, dropout=dropout)
+        self.encoder3 = UBlock(features[1], features[2] // 2, features[2], norm_layer, dropout=dropout)
+        self.encoder4 = UBlock(features[2], features[3] // 2, features[3], norm_layer, dropout=dropout)
+
+        self.bottom = UBlock(features[3], features[3], features[3], norm_layer, (2, 2), dropout=dropout)
+
+        self.bottom_2 = ConvBnRelu(features[3] * 2, features[2], norm_layer, dropout=dropout)
+
+        self.downsample = nn.MaxPool3d(2, 2)
+
+        self.decoder3 = UBlock(features[2] * 2, features[2], features[1], norm_layer, dropout=dropout)
+        self.decoder2 = UBlock(features[1] * 2, features[1], features[0], norm_layer, dropout=dropout)
+        self.decoder1 = UBlock(features[0] * 2, features[0], features[0] // 2, norm_layer, dropout=dropout)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
+
+        self.outconv = conv1x1(features[0] // 2, num_classes)
+
+        if self.deep_supervision:
+            self.deep_bottom = nn.Sequential(
+                conv1x1(features[3], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep_bottom2 = nn.Sequential(
+                conv1x1(features[2], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep3 = nn.Sequential(
+                conv1x1(features[1], num_classes),
+                nn.Upsample(scale_factor=4, mode="trilinear", align_corners=True))
+
+            self.deep2 = nn.Sequential(
+                conv1x1(features[0], num_classes),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True))
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv3d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm3d, nn.GroupNorm, nn.InstanceNorm3d)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+
+        down1 = self.encoder1(x)
+        down2 = self.downsample(down1)
+        down2 = self.encoder2(down2)
+        down3 = self.downsample(down2)
+        down3 = self.encoder3(down3)
+        down4 = self.downsample(down3)
+        down4 = self.encoder4(down4)
+
+        bottom = self.bottom(down4)
+        bottom_2 = self.bottom_2(torch.cat([down4, bottom], dim=1))
+
+        # Decoder
+
+        up3 = self.upsample(bottom_2)
+        up3 = self.decoder3(torch.cat([down3, up3], dim=1))
+        up2 = self.upsample(up3)
+        up2 = self.decoder2(torch.cat([down2, up2], dim=1))
+        up1 = self.upsample(up2)
+        up1 = self.decoder1(torch.cat([down1, up1], dim=1))
+
+        out = self.outconv(up1)
+
+        if self.deep_supervision:
+            deeps = []
+            for seg, deep in zip(
+                    [bottom, bottom_2, up3, up2],
+                    [self.deep_bottom, self.deep_bottom2, self.deep3, self.deep2]):
+                deeps.append(deep(seg))
+            return out, deeps
+
+        return out
+
+
+class EquiUnet(Unet):
+    """Almost the most basic U-net: all Block have the same size if they are at the same level.
+    """
+    name = "EquiUnet"
+
+    def __init__(self, inplanes, num_classes, width, norm_layer=None, deep_supervision=False, dropout=0,
+                 **kwargs):
+        super(Unet, self).__init__()
+        features = [width * 2 ** i for i in range(4)]
+        print(features)
+
+        self.deep_supervision = deep_supervision
+
+        self.encoder1 = UBlock(inplanes, features[0], features[0], norm_layer, dropout=dropout)
+        self.encoder2 = UBlock(features[0], features[1], features[1], norm_layer, dropout=dropout)
+        self.encoder3 = UBlock(features[1], features[2], features[2], norm_layer, dropout=dropout)
+        self.encoder4 = UBlock(features[2], features[3], features[3], norm_layer, dropout=dropout)
+
+        self.bottom = UBlock(features[3], features[3], features[3], norm_layer, (2, 2), dropout=dropout)
+
+        self.bottom_2 = ConvBnRelu(features[3] * 2, features[2], norm_layer, dropout=dropout)
+
+        self.downsample = nn.MaxPool3d(2, 2)
+
+        self.decoder3 = UBlock(features[2] * 2, features[2], features[1], norm_layer, dropout=dropout)
+        self.decoder2 = UBlock(features[1] * 2, features[1], features[0], norm_layer, dropout=dropout)
+        self.decoder1 = UBlock(features[0] * 2, features[0], features[0], norm_layer, dropout=dropout)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
+
+        self.outconv = conv1x1(features[0], num_classes)
+
+        if self.deep_supervision:
+            self.deep_bottom = nn.Sequential(
+                conv1x1(features[3], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep_bottom2 = nn.Sequential(
+                conv1x1(features[2], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep3 = nn.Sequential(
+                conv1x1(features[1], num_classes),
+                nn.Upsample(scale_factor=4, mode="trilinear", align_corners=True))
+
+            self.deep2 = nn.Sequential(
+                conv1x1(features[0], num_classes),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True))
+
+        self._init_weights()
+
+    def forward(self, x):
+
+        down1 = self.encoder1(x)
+        down2 = self.downsample(down1)
+        down2 = self.encoder2(down2)
+        down3 = self.downsample(down2)
+        down3 = self.encoder3(down3)
+        down4 = self.downsample(down3)
+        down4 = self.encoder4(down4)
+
+        bottom = self.bottom(down4)
+        bottom_2 = self.bottom_2(torch.cat([down4, bottom], dim=1))
+
+        # Decoder
+
+        up3 = self.upsample(bottom_2)
+        up3 = self.decoder3(torch.cat([down3, up3], dim=1))
+        up2 = self.upsample(up3)
+        up2 = self.decoder2(torch.cat([down2, up2], dim=1))
+        up1 = self.upsample(up2)
+        up1 = self.decoder1(torch.cat([down1, up1], dim=1))
+
+        out = self.outconv(up1)
+
+        if self.deep_supervision:
+            deeps = []
+            for seg, deep in zip(
+                    [bottom, bottom_2, up3, up2],
+                    [self.deep_bottom, self.deep_bottom2, self.deep3, self.deep2]):
+                deeps.append(deep(seg))
+            return out, deeps
+
+        return out
+
+
+class Att_EquiUnet(Unet):
+    def __init__(self, inplanes = 16, num_classes = 4 , width = 16 , norm_layer=None, deep_supervision=False,  dropout=0,
+                 **kwargs):
+        super(Unet, self).__init__()
+        features = [width * 2 ** i for i in range(5)]
+        print(features)
+
+        self.deep_supervision = deep_supervision
+        self.InitConv = InitConv(in_channels=4, out_channels=16, dropout=0.2)
+
+        self.encoder1 = UBlockCbam(inplanes, features[0], features[0], norm_layer, dropout=dropout)
+        self.encoder2 = UBlockCbam(features[0], features[1], features[1], norm_layer, dropout=dropout)
+        self.encoder3 = UBlockCbam(features[1], features[2], features[2], norm_layer, dropout=dropout)
+        self.encoder4 = UBlockCbam(features[2], features[3], features[3], norm_layer, dropout=dropout)
+
+        self.bottom = UBlockCbam(features[3], features[4], features[4], norm_layer, (2, 2), dropout=dropout)
+
+        self.bottom_2 = nn.Sequential(
+            ConvBnRelu(features[3] * 2, features[2], norm_layer, dropout=dropout),
+            CBAM(features[2], norm_layer=norm_layer)
+        )
+
+        self.downsample = nn.MaxPool3d(2, 2)
+
+        self.decoder3 = UBlock(features[2] * 2, features[2], features[1], norm_layer, dropout=dropout)
+        self.decoder2 = UBlock(features[1] * 2, features[1], features[0], norm_layer, dropout=dropout)
+        self.decoder1 = UBlock(features[0] * 2, features[0], features[0], norm_layer, dropout=dropout)
+
+        self.upsample = nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True)
+
+        self.outconv = conv1x1(features[0], num_classes)
+
+        if self.deep_supervision:
+            self.deep_bottom = nn.Sequential(
+                conv1x1(features[3], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep_bottom2 = nn.Sequential(
+                conv1x1(features[2], num_classes),
+                nn.Upsample(scale_factor=8, mode="trilinear", align_corners=True))
+
+            self.deep3 = nn.Sequential(
+                conv1x1(features[1], num_classes),
+                nn.Upsample(scale_factor=4, mode="trilinear", align_corners=True))
+
+            self.deep2 = nn.Sequential(
+                conv1x1(features[0], num_classes),
+                nn.Upsample(scale_factor=2, mode="trilinear", align_corners=True))
+
+        self._init_weights()
+        
+    def forward(self, x):
+        x = self.InitConv(x)       # (1, 16, 128, 128, 128)
+        
+        x1_1 = self.encoder1(x)
+        x1_2 = self.downsample(x1_1) # (1, 16, 64, 64, 64)
+        x2_1 = self.encoder2(x1_2)
+        x2_2 = self.downsample(x2_1) # (1, 32, 32, 32, 32)
+        x3_1 = self.encoder3(x2_2)
+        x3_2 = self.downsample(x3_1) # (1, 64, 16, 16, 16)
+        x4_1 = self.encoder4(x3_2)
+        x4_2 = self.downsample(x4_1) # (1, 128, 1
+         
+        output = self.bottom(x4_2) # (1, 256, 8, 8, 8)
+        
+
+        return x1_1,x2_1,x3_1,x4_1,output
+
+if __name__ == '__main__':
+    with torch.no_grad():
+        import os
+        os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+        cuda0 = torch.device('cuda:0')
+        x = torch.rand((1, 4, 128, 128, 128), device=cuda0)
+        # model = Unet1(in_channels=4, base_channels=16, num_classes=4)
+        model = Att_EquiUnet(inplanes = 16, num_classes = 4 , width = 16, norm_layer=None, deep_supervision=False,  dropout=0,
+                 **kwargs)
+        model.cuda()
+        output = model(x)
+        print('output:', output.shape)
